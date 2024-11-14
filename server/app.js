1bbaa79638dd7d1d7e1059038cec5a4fd5b51b2e
Gnani1793 <gnanananda.22bce20031@vitapstudent.ac.in>
2024-11-15 01:23:26 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.post('/submitPatientData', async (req, res) => {
 });
 
 // Start the server
-const PORT = 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
